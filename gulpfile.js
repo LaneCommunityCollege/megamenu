fa6bc61ec773fc40d115a89f826a679cc8759fab
Kyle Schmidt <schmidtk@lanecc.edu>
2015-10-06 15:14:19 -0500

    standardized tabbing

--- gulpfile.js
+++ gulpfile.js
@@ -11,15 +11,15 @@ var gulp = require('gulp'),
     jade = require('gulp-jade');
 
 gulp.task('minify-html', function(){
-    var opts = {
-        conditionals: true,
-        quotes: true,
-        empty: true
-    };
+  var opts = {
+      conditionals: true,
+      quotes: true,
+      empty: true
+  };
 
-    return gulp.src('tmp/menu.html')
-        .pipe(minifyHTML(opts))
-        .pipe(gulp.dest('tmp/'));
+  return gulp.src('tmp/menu.html')
+      .pipe(minifyHTML(opts))
+      .pipe(gulp.dest('tmp/'));
 });
 
 gulp.task('compass', function() {
@@ -67,21 +67,21 @@ gulp.task('inject-css', function(){
 });
 
 gulp.task('compress', function(){
-    return gulp.src('dist/js/lmm.js')
-      .pipe(uglify())
-      .pipe(rename({suffix:".min"}))
-      .pipe(gulp.dest('dist/js/'));
+  return gulp.src('dist/js/lmm.js')
+    .pipe(uglify())
+    .pipe(rename({suffix:".min"}))
+    .pipe(gulp.dest('dist/js/'));
 });
 
 gulp.task('clean', function(cb) {
-    del('tmp', cb);
+  del('tmp', cb); 
 });
 
 gulp.task('build', function(cb){
-    runSequence(['build-html', 'compass'],
-                ['minify-css', 'minify-html'],
-                 'inject-html', 
-                 'inject-css',
-                 'compress',
-                 'clean'); 
+  runSequence(['build-html', 'compass'],
+              ['minify-css', 'minify-html'],
+               'inject-html', 
+               'inject-css',
+               'compress',
+               'clean'); 
 });
