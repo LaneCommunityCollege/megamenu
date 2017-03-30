3d3f352dddab21c7e7d8a3f574628b5c8bfcd197
Kyle Schmidt <schmidtk@lanecc.edu>
2017-03-30 16:45:43 -0700

    swap load to domonload, fix #33

--- src/js/lmm.dev.js
+++ src/js/lmm.dev.js
@@ -1,15 +1,15 @@
 "use strict";
 
-window.onload = function() {
+document.addEventListener("DOMContentLoaded", function() {
     /* If someone includes the script twice (which has happened), the mega menu throws
      * open and close events simultaneously, meaning it doesn't work at all. So here
      * we'll make sure we don't accidentally draw it twice. */
-    if(typeof mm !== 'undefined'){
+    if(typeof window.mm !== 'undefined'){
         console.log("megamenu script already present");
         return;
     }
     else {
-        var mm = "loaded";
+        window.mm = "loaded";
     }
 
     if(!('classList' in document.body))
@@ -326,11 +326,13 @@ window.onload = function() {
         if(window.innerWidth >= 1440){
             width = 1180;
         }
+
         let leftmargin = Math.floor(window.innerWidth - width) / 2 - 14;
         //keeps us from sliding under the logo
         if(leftmargin < 0){
             leftmargin = 0;
         }
+
         $cats.style.marginLeft = leftmargin + "px";
 
         if(visible($lmm.getElementsByClassName('lmm-searchops')[0])){
@@ -353,4 +355,4 @@ window.onload = function() {
     }
     onResize();
     window.addEventListener('resize', onResize);
-};
+});
